828a1f30127deb1eb82d997f255d48c5a06b3220
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2017-10-02 09:37:14 -0300

    testing click options :notes:

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,6 @@
 var body = document.getElementsByTagName('body');
 var element = body[0];
 
-element.onclick = function() {
-  this.classList.toggle('teste');
-};
+// element.onclick = function() {
+//   this.classList.toggle('teste');
+// };
